8cde4878cb81a1117204fb30e94bb77ebfeca278
dharna <dharnaahuja123@gmail.com>
2026-03-12 06:52:13 +0530

    added env variable example

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
-
+import os
 app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World"
+    app_name=os.environ['APP_NAME']
+
+    return f"Welcome to {app_name}"
 
 if __name__=="__main__":
     app.run(debug=True)
